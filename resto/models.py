6121eec8f58dd92728717e69feaf4602b1199b15
Mbogo47 <njokiivy47@gmail.com>
2023-11-28 14:34:31 +0300

    Made models for tables

--- resto/models.py
+++ resto/models.py
@@ -1,23 +1,22 @@
 from django.db import models
 
-# Create your models here.
 class Booking(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
-    date = models.DateField()
-    time = models.TimeField()
-    people = models.PositiveIntegerField()
-    message = models.TextField()
+    booking_name = models.CharField(max_length=255)
+    booking_email = models.EmailField()
+    booking_phone = models.CharField(max_length=15)
+    booking_date = models.DateField()
+    booking_time = models.TimeField()
+    booking_people = models.PositiveIntegerField()
+    booking_message = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.booking_name
 
 class ContactMessage(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    subject = models.CharField(max_length=255)
-    message = models.TextField()
+    contact_name = models.CharField(max_length=255)
+    contact_email = models.EmailField()
+    contact_subject = models.CharField(max_length=255)
+    contact_message = models.TextField()
 
     def __str__(self):
-        return f"{self.name} - {self.subject}"
+        return f"{self.contact_name} - {self.contact_subject}"
--- resto/views.py
+++ resto/views.py
@@ -47,24 +47,24 @@ def index(request):
 
     # Handle Contact Form Submission
     if request.method == 'POST' and 'contact_submit' in request.POST:
-        name = request.POST['name']
-        email = request.POST['email']
-        subject = request.POST['subject']
-        message = request.POST['message']
-        contact = ContactMessage(name=name, email=email, subject=subject, message=message)
+        contact_name = request.POST['contact_name']
+        contact_email = request.POST['contact_email']
+        contact_subject = request.POST['contact_subject']
+        contact_message = request.POST['contact_message']
+        contact = ContactMessage(contact_name=contact_name, contact_email=contact_email, contact_subject=contact_subject, contact_message=contact_message)
         contact.save()
         contactus = ContactMessage.objects.all()  # Refresh the data after submission
 
     # Handle Booking Form Submission
     elif request.method == 'POST' and 'booking_submit' in request.POST:
-        name = request.POST['name']
-        email = request.POST['email']
-        phone = request.POST['phone']
-        date = request.POST['date']
-        time = request.POST['time']
-        people = request.POST['people']
-        message = request.POST['message']
-        booking = Booking(name=name, email=email, phone=phone, date=date, time=time, people=people, message=message)
+        booking_name = request.POST['booking_name']
+        booking_email = request.POST['booking_email']
+        booking_phone = request.POST['booking_phone']
+        booking_date = request.POST['booking_date']
+        booking_time = request.POST['booking_time']
+        booking_people = request.POST['booking_people']
+        booking_message = request.POST['booking_message']
+        booking = Booking(booking_name=booking_name, booking_email=booking_email, booking_phone=booking_phone, booking_date=booking_date, booking_time=booking_time, booking_people=booking_people, booking_message=booking_message)
         booking.save()
         bookings = Booking.objects.all()  # Refresh the data after submission
 
